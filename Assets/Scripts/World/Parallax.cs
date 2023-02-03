f364ee777218d17c50d7ce252ba877b9435084a0
Slash <slash@slashware.net>
2023-02-02 21:31:54 -0500

    Colored parallax layers with uneven width

--- Assets/Scripts/World/Parallax.cs
+++ Assets/Scripts/World/Parallax.cs
@@ -7,13 +7,22 @@ public class Parallax : MonoBehaviour
     public GameObject mirror;
     public float speed;
 
+    private float imageWidth;
+
+    private const float HALF_SCREEN = 19.2f / 2.0f;
+
+    void Start () {
+        imageWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        transform.Translate(-HALF_SCREEN, 0, 0);
+    }
+
     void Update() {
         if (World.i.stopTime) {
             return;
         }
         transform.Translate(-speed * Time.deltaTime, 0, 0);
-        if (transform.position.x <= -19.2) {
-            transform.Translate(19.2f * 2, 0, 0);
+        if (transform.position.x <= -imageWidth - HALF_SCREEN) {
+            transform.Translate(imageWidth * 2, 0, 0);
         }
     }
 }
