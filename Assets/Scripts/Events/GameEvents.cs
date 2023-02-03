ab1b828485b74a99dd32b63392ef566885fe1665
Slash <slash@slashware.net>
2023-02-03 12:24:08 -0500

    Stub for some events

--- Assets/Scripts/Events/GameEvents.cs
+++ Assets/Scripts/Events/GameEvents.cs
@@ -6,6 +6,21 @@ public static class GameEvents {
 		new GameEvent() { id = "militia", prompt = "Un grupo armado te detiene, su lider pregunta:\n“Qué tenemos aquí? Reconozco chulavitas con solo verlos!“", options = new GameEventOption[] {
 			new GameEventOption() { description = "No somos chulavitas! Por favor déjame pasar con mi familia!" }
 		}},
+		new GameEvent() {
+			id = "stolenAnimal",
+			prompt = "After a good night’s sleep, you notice XXX is missing. After spending several hours looking for it, you decide it’s time to move on.",
+			options = new GameEventOption[] {
+				new GameEventOption() { description = "I should have been more careful." }
+			}
+		},
+		new GameEvent() {
+			id = "cart",
+			prompt = "Ahead on the road you see what appears to be a cart. It has some food inside it that seems edible. The surrounding soil is covered by blood. There are no mules or horses, and no one answer your calls.",
+			options = new GameEventOption[] {
+				new GameEventOption() { description = "Load Rosita with all the food. [+20 food]" },
+				new GameEventOption() { description = "Leave some food behind. Someone else may need it. [+10 food]" }
+			}
+		},
 		new GameEvent() { id = "house", prompt = "Tu familia se acerca a una casa, parece estar abandonada. Qué quieres hacer?", options = new GameEventOption[] {
 			new GameEventOption() { description = "Asaltar la casa" },
 			new GameEventOption() { description = "Seguir por el camino" },
